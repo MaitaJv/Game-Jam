c7124b697b1efeab081bc063627d007858478fb8
Javier Maita <javiermaita@hotmail.com.ar>
2025-10-12 13:13:19 -0300

    Revert "Add files via upload"
    This reverts commit a99bc0be982c92ffdfb28bc10ecd087a2edba0c1.

--- main.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-@onready var planeta = $Planeta
-
-func _ready():
-	planeta.
--- planeta.gd
+++
@@ -1,5 +0,0 @@
-extends RigidBody2D
-signal desaparecer 
-
-func _on_body_entered() -> void:
-	desaparecer.emit()
